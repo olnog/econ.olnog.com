18424dc62398a6d792d66e8b9af36f8498add1ee
olnog@gmail.com <therealolnog@gmail.com>
2022-05-04 02:39:49 +0000

    Added repair icons to buildings page

--- resources/views/Buildings/index.blade.php
+++ resources/views/Buildings/index.blade.php
@@ -33,12 +33,20 @@
    <div id='buildingWarning' class='text-decoration-underline text-center'></div>
    <!--<input type='checkbox' id='filterFields' class='filterBuildings'> Hide Fields?-->
  </div><div id='buildingListings' class='p-3'>
-
+{{var_dump($repairable)}}
 @foreach($buildings as $building)
   @continue (in_array($building->name, $fieldsDisplayed))
   <div class='mt-3 @if ($building->farming) fields @endif'>
     <div>
+      @if ($building->uses < $building->totalUses
+        && in_array($building->id, $repairable ))
+        <button id='repair-{{$building->id}}' class='repair btn'>
+          <img src='/img/icons8-job-30.png'>
+        </button>
+
+      @endif
       <span class='@if ($building->uses == 0 && !$building->farming) text-danger @endif '>
+
        {{$building->name}}
        @if (!$building->farming)
         {{round($building->uses / $building->totalUses * 100, 2)}}%
@@ -52,6 +60,7 @@
       class='show btn btn-link'>+</button>
     <button id='hide-buildingButtons{{$building->id}}'
       class='hide btn btn-link d-none'>-</button>
+
 
     </div><div id='buildingButtons{{$building->id}}' class='d-none'>
       <button id='destroyBuilding-{{$building->id}}'
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,7 +82,7 @@
       <a target="_blank" href="https://icons8.com/icon/Qpt0cBZAp1GC/pacifier">Pacifier</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
       <a target="_blank" href="https://icons8.com/icon/61247/rent">Rent</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
       <a target="_blank" href="https://icons8.com/icon/hCFmpYC60qKc/feedback">Feedback</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-
+      <a target="_blank" href="https://icons8.com/icon/61517/job">Job</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
     </div><div>
       <a href='https://freesound.org/people/suzenako/sounds/320905/'>The Ding</a>
       <a href='https://freesound.org/people/InspectorJ/sounds/415510/'>Bell, Counter,</a>
